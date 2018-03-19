b5c8d659495f41b8ba246883f60187f01b140ee8
Will Trimble <trimble@anl.gov>
2018-03-19 15:38:20 -0500

    Added test for seq stats calculation, doesn't wait though

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -38,3 +38,24 @@ def test_upload_and_validate():
     print(o2)
     j2 = json.loads(o2)
     assert j2["status"] == "valid metadata"
+
+
+def test_fastq_upload_and_validate():
+    if 'MGRKEY' in os.environ:
+        MGRKEY = os.environ['MGRKEY']
+    else: 
+        assert False, "This test does not work without MGRKEY"
+    CMD = '''curl -X POST -H "Authorization: mgrast {}" -F "upload=@data/Sample.DM.fastq.gz" "https://api.mg-rast.org/inbox"'''.format(MGRKEY)
+    print(CMD)
+    o = check_output(CMD, shell=True)
+    j = json.loads(o)    
+    ID = re.findall("\((.*)\)", j["status"])[0]
+    print(ID)
+    assert "Sample.DM.fastq" in j["status"]
+    CMD = '''curl -X GET -H "Authorization: mgrast {}" "https://api.mg-rast.org/inbox/stats/{}"'''.format(MGRKEY, ID)
+    o2 = check_output(CMD, shell=True)
+    print(o2)
+    j2 = json.loads(o2)
+    assert "status" in j2.keys()
+    assert "timestamp" in j2.keys()
+    assert "awe_id" in j2.keys()
