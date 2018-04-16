a87b13f503b4c079ce41de75f94eabfee037dd70
Will Trimble <trimble@anl.gov>
2018-04-16 12:41:29 -0500

    Removed silent option from curl, replaced with suppress progress bar

--- tests/test_byhand.py
+++ tests/test_byhand.py
@@ -19,63 +19,63 @@ APIS = read_api_list("API.server.list")
 @pytest.mark.parametrize("API_URL", APIS)
 def test_utf8_project(API_URL):
     URL = API_URL + '/project/mgp128?verbosity=full'
-    a = check_output('''curl -s '{}' |file -'''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' |file -'''.format(URL), shell=True)
     assert b"UTF-8" in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_utf8_metagenome_export(API_URL):
     URL = API_URL + "/metagenome/mgm4447943.3?verbosity=metadata"
-    a = check_output('''curl -s '{}' | file -'''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' | file -'''.format(URL), shell=True)
     assert b"UTF-8" in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_utf8_metadata_export(API_URL):
     URL = API_URL + "/metadata/export/mgp128"
-    a = check_output('''curl -s '{}' | file -'''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' | file -'''.format(URL), shell=True)
     assert b"UTF-8" in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_download_partial(API_URL):
     URL = API_URL + "/download/mgm4447943.3?file=350.1"
-    a = check_output('''curl -s '{}' | head -n 100 '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' | head -n 100 '''.format(URL), shell=True)
     assert b"ERROR" not in a
     assert b"GF8803K01A00MJ" in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_annotation_similarity(API_URL):
     URL = API_URL + "/annotation/similarity/mgm4447943.3?identity=80&type=function&source=KO"
-    a = check_output('''curl -s '{}' | head -n 10 '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' | head -n 10 '''.format(URL), shell=True)
     assert b"ERROR" not in a
     assert b"\t" in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_annotation_sequence(API_URL):
     URL = API_URL + "/annotation/sequence/mgm4447943.3?evalue=10&type=organism&source=SwissProt"
-    a = check_output('''curl -s '{}' | head -n 10 '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' | head -n 10 '''.format(URL), shell=True)
     assert b"ERROR" not in a
     assert b"\t" in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_blast_result_http(API_URL):
     URL = API_URL + "/compute/blast/mgm4447943.3?md5=0001c08aa276d154b7696f9758839786"
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
     assert b"alignment" in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_blast_result_https(API_URL):
     URL = API_URL + "/compute/blast/mgm4447943.3?md5=0001c08aa276d154b7696f9758839786"
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
     assert b"alignment" in a
 
 #def test_post1(API_URL):
-#    CMD = '''curl -X POST -d '{"source":"KO","type":"function","md5s":["000821a2e2f63df1a3873e4b280002a8","15bf1950bd9867099e72ea6516e3d602"]}' "https://api.mg-rast.org/annotation/sequence/mgm4447943.3"'''     # EMPTY RETURN DATA
+#    CMD = '''curl -sS -X POST -d '{"source":"KO","type":"function","md5s":["000821a2e2f63df1a3873e4b280002a8","15bf1950bd9867099e72ea6516e3d602"]}' "https://api.mg-rast.org/annotation/sequence/mgm4447943.3"'''     # EMPTY RETURN DATA
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_post2(API_URL):
     URL = API_URL + "/annotation/sequence/mgm4447943.3"
-    CMD = '''curl -s -X POST -d '{"source":"SwissProt","type":"organism","md5s":["000821a2e2f63df1a3873e4b280002a8","15bf1950bd9867099e72ea6516e3d602"]}' "''' + API_URL + '''"'''
+    CMD = '''curl -sS -X POST -d '{"source":"SwissProt","type":"organism","md5s":["000821a2e2f63df1a3873e4b280002a8","15bf1950bd9867099e72ea6516e3d602"]}' "''' + API_URL + '''"'''
     a = check_output(CMD, shell=True)
     assert b"ERROR" not in a
 
@@ -83,58 +83,58 @@ def test_post2(API_URL):
 def test_errs_matrix_function(API_URL):
     URL = API_URL + "/matrix/function?id=mgm4447943.3&id=mgm4447192.3&id=mgm4447102.3&group_level=level3&source=Subsystems&identity=80"
     print(URL)
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
 @pytest.mark.parametrize("API_URL", APIS)
 def test_errs_profile(API_URL):
     URL = API_URL + "/profile/mgm4447943.3?source=RefSeq&format=biom"
     print(URL)
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
 @pytest.mark.parametrize("API_URL", APIS)
 def test_errs_matrix_organism(API_URL):
     URL = API_URL + "/matrix/organism?id=mgm4447943.3&id=mgm4447192.3&id=mgm4447102.3&group_level=family&source=RefSeq&evalue=15"
     print(URL)
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
 @pytest.mark.parametrize("API_URL", APIS)
 def test_errs_library(API_URL):
     URL = API_URL + "/library/mgl52924?verbosity=full"
     print(URL)
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
 @pytest.mark.parametrize("API_URL", APIS)
 def test_errs_metadata(API_URL):
     URL = API_URL + "/metadata/ontology?name=biome&version=2017-04-15"
     print(URL)
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
 @pytest.mark.parametrize("API_URL", APIS)
 def test_errs_darkmatter(API_URL):
     URL = API_URL + "/darkmatter/mgm4447943.3?"
     print(URL)
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
 @pytest.mark.parametrize("API_URL", APIS)
 def test_errs_download_history(API_URL):
     URL = API_URL + "/download/history/mgm4447943.3"
     print(URL)
-    a = check_output('''curl -s '{}' '''.format(URL), shell=True)
+    a = check_output('''curl -sS '{}' '''.format(URL), shell=True)
     assert b"ERROR" not in a
 @pytest.mark.parametrize("API_URL", APIS)
 def test_err_post_parsing_err(API_URL):
-    CURLCMD = '''curl -d '{"data":"000821a2e2f63df1a3873e4b280002a8","format":"fasta","sequence":0,"source":"InterPro"}' ''' + API_URL + '''/m5nr/md5'''
+    CURLCMD = '''curl -sS -d '{"data":"000821a2e2f63df1a3873e4b280002a8","format":"fasta","sequence":0,"source":"InterPro"}' ''' + API_URL + '''/m5nr/md5'''
     a = check_output(CURLCMD, shell=True)
     assert b"ARRAY ref" not in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_err_post_parsing_correct(API_URL):
-    CURLCMD = '''curl -d '{"data":["000821a2e2f63df1a3873e4b280002a8"],"format":"fasta","sequence":0,"source":"InterPro"}' ''' + API_URL + '''/m5nr/md5'''
+    CURLCMD = '''curl -sS -d '{"data":["000821a2e2f63df1a3873e4b280002a8"],"format":"fasta","sequence":0,"source":"InterPro"}' ''' + API_URL + '''/m5nr/md5'''
     a = check_output(CURLCMD, shell=True)
     assert b"ARRAY ref" not in a
 
 @pytest.mark.parametrize("API_URL", APIS)
 def test_err_parse_md5_blast(API_URL):
-    CURLCMD = "curl "  + API_URL + "/compute/blast/mgm4447943.3?asynchronous=0&md5=15bf1950bd9867099e72ea6516e3d602&rna=0" 
+    CURLCMD = "curl -sS "  + API_URL + "/compute/blast/mgm4447943.3?asynchronous=0&md5=15bf1950bd9867099e72ea6516e3d602&rna=0" 
     a = check_output(CURLCMD, shell=True)
     assert b"ERROR" not in a
