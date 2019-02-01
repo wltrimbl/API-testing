2e2a87a0e95c2b18897d0adf2918bf70bed4ebb5
Will Trimble <trimble@anl.gov>
2019-02-01 10:37:49 -0600

    Decorators for project_updatemetadata

--- tests/test_project_updatemetadata.py
+++ tests/test_project_updatemetadata.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import os
 import json
 import requests
+import pytest
 
 if 'MGRKEY' in os.environ:
     MGRKEY = os.environ['MGRKEY']
@@ -21,7 +22,7 @@ def get_proj_metadata(proj, field):
     aa = json.loads(a.content.decode("utf-8"))
     assert "ERROR" not in a.content.decode("utf-8")
     return(field, aa["metadata"][field])
-
+@pytest.mark.requires_auth
 def set_proj_metadata(proj, terms):
     '''Connect to MG-RAST API, authorize, and change one or more fields
     of project metadata.'''
@@ -34,11 +35,31 @@ def set_proj_metadata(proj, terms):
     b = requests.post(SETURL, headers=SETHEADERS, files=SETDATA)
     print(b.content.decode("utf-8"))
 
+@pytest.mark.requires_auth
+def test_project_updatemetadata():
+    '''Update a field of project metadata, confirm its value,
+    update it again (with utf-8) and confirm its value.'''
+    PROJECT = "mgp34834"  # This is a garbage project
+    FIELD = "organization_url"
+    set_proj_metadata(PROJECT, {FIELD: "TESTING"})
+    f, v = get_proj_metadata(PROJECT, FIELD)
+    print(f, v)
+    assert v == "TESTING"
+    set_proj_metadata(PROJECT, {FIELD: "TESTING2"})
+    f, v = get_proj_metadata(PROJECT, FIELD)
+    print(f, v)
+    assert v == "TESTING2"
+
+@pytest.mark.requires_auth
 def test_project_updatemetadata_with_utf8():
     '''Update a field of project metadata, confirm its value,
     update it again (with utf-8) and confirm its value.'''
     PROJECT = "mgp34834"  # This is a garbage project
     FIELD = "organization_url"
+    set_proj_metadata(PROJECT, {FIELD: "TESTING"})
+    f, v = get_proj_metadata(PROJECT, FIELD)
+    print(f, v)
+    assert v == "TESTING"
     set_proj_metadata(PROJECT, {FIELD: "TESTING2"})
     f, v = get_proj_metadata(PROJECT, FIELD)
     print(f, v)
