f643cd4c234d48432eaa3b053d03ec9064aae7d1
Will Trimble <trimble@anl.gov>
2019-02-20 09:55:52 -0600

    Added sleep to test_cert

--- tests/test_cert.py
+++ tests/test_cert.py
@@ -1,3 +1,5 @@
+from time import sleep 
+
 try:
     import urllib2 #python2
 except:
@@ -8,16 +10,20 @@ HEADERS = {'User-Agent':'Mozilla/5.0'}
 def test_cert_shock():
     req = urllib2.Request("https://shock.mg-rast.org", headers=HEADERS)
     urllib2.urlopen(req)
+    sleep(5)
 
 def test_cert_awe():
     req = urllib2.Request("https://awe.mg-rast.org", headers=HEADERS)
     urllib2.urlopen(req)
+    sleep(5)
 
 def test_cert_mgrast():
     req = urllib2.Request("https://mg-rast.org", headers=HEADERS)
     urllib2.urlopen(req)
+    sleep(5)
 
 def test_cert_api():
     req = urllib2.Request("https://api.mg-rast.org", headers=HEADERS)
     urllib2.urlopen(req)
+    sleep(5)
 
