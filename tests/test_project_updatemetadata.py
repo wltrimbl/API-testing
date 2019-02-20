ad42a635b3f5eeddffc07036e208db9a4e1840ea
Will Trimble <trimble@anl.gov>
2019-02-20 09:54:20 -0600

    Parameterized tests for API_URL

--- tests/test_project_updatemetadata.py
+++ tests/test_project_updatemetadata.py
@@ -15,7 +15,7 @@ else:
     assert False, "This test does not work without MGRKEY"
 
 
-def get_proj_metadata(proj, field):
+def get_proj_metadata(proj, field, API_URL):
     '''Connect to MG-RAST API, authorize, and read project metadata.'''
     TESTURL = API_URL + "/project/{PROJ}".format(PROJ=proj)
     TESTDATA = {"verbosity":"full", "nocache":1}
@@ -26,7 +26,7 @@ def get_proj_metadata(proj, field):
     assert "ERROR" not in a.content.decode("utf-8")
     return(field, aa["metadata"][field])
 
-def set_proj_metadata(proj, terms):
+def set_proj_metadata(proj, terms, API_URL):
     '''Connect to MG-RAST API, authorize, and change one or more fields
     of project metadata.'''
     SETURL = API_URL + "/project/{PROJ}/updatemetadata".format(PROJ=proj)
@@ -45,12 +45,12 @@ def test_project_updatemetadata(API_URL):
     update it again (with utf-8) and confirm its value.'''
     PROJECT = "mgp34834"  # This is a garbage project
     FIELD = "organization_url"
-    set_proj_metadata(PROJECT, {FIELD: "TESTING"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "TESTING"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "TESTING"
-    set_proj_metadata(PROJECT, {FIELD: "TESTING2"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "TESTING2"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "TESTING2"
 
@@ -62,16 +62,16 @@ def test_project_updatemetadata_with_utf8(API_URL):
     update it again (with utf-8) and confirm its value.'''
     PROJECT = "mgp34834"  # This is a garbage project
     FIELD = "organization_url"
-    set_proj_metadata(PROJECT, {FIELD: "TESTING"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "TESTING"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "TESTING"
-    set_proj_metadata(PROJECT, {FIELD: "TESTING2"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "TESTING2"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "TESTING2"
-    set_proj_metadata(PROJECT, {FIELD: "Tésting2"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "Tésting2"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "Tésting2"
 
@@ -83,15 +83,15 @@ def test_project_updatemetadata_with_utf8_U_0634(API_URL):
     update it again (with utf-8) and confirm its value.'''
     PROJECT = "mgp34834"  # This is a garbage project
     FIELD = "organization_url"
-    set_proj_metadata(PROJECT, {FIELD: "TESTING"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "TESTING"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "TESTING"
-    set_proj_metadata(PROJECT, {FIELD: "TESTING2"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "TESTING2"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "TESTING2"
-    set_proj_metadata(PROJECT, {FIELD: "Testing ش"})
-    f, v = get_proj_metadata(PROJECT, FIELD)
+    set_proj_metadata(PROJECT, {FIELD: "Testing ش"}, API_URL)
+    f, v = get_proj_metadata(PROJECT, FIELD, API_URL)
     print(f, v)
     assert v == "Testing ش"
